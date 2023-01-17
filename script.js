2336cedfd59d5cdfddf8baddad5e54634c26c2d9
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-01-17 10:03:46 -0600

    Completed bonus section

--- script.js
+++ script.js
@@ -32,3 +32,19 @@ console.log(blank); // undefined
 // 9
 blank = null;
 console.log(blank); // null
+
+// BONUS
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// 2
+console.log(quarter, dime, nickel, penny); // 0.25 0.1 0.05 0.01
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+console.log(letters[letters.length - 1]); // p
+// console.log(letters[10 - 1]);
+// console.log(letters[9]);
+// console.log("p");
